8bcf041652dc3eb5ed65dc368ba79a35a18a9b33
nv21070-mohammed hussain <NV21070@nvtc.edu.bh>
2022-12-26 10:15:01 +0300

    updated name v1

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -251,7 +251,7 @@ export default function Home({ articles }) {
     <>
       <Head>
         <title>
-          Mohammed Hussain - Software designer at NVTC
+          Mohammed Hussain, Software designer at NVTC
         </title>
         <meta
           name="description"
@@ -261,33 +261,34 @@ export default function Home({ articles }) {
       <Container className="mt-9">
         <div className="max-w-2xl">
           <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
-            Software designer, founder.
+            Software designer, founder, an amateur astraunt.
           </h1>
           <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
-            I’m Mohammed, a software designer and entrepreneur based in NVTC. I’m the founder and CEO of DEEZ, where we develop
-            technologies that empower regular people to explore nearby on their
+            I’m Mohammed, a software designer and entrepreneur based in New York
+            City. I’m the founder and CEO of DEEZ, where we develop
+            technologies that empower regular people to explore space on their
             own terms.
           </p>
           <div className="mt-6 flex gap-6">
             <SocialLink
               href="https://twitter.com"
               aria-label="Follow on Twitter"
-              icon={DEEZIcon}
+              icon={TwitterIcon}
             />
             <SocialLink
               href="https://instagram.com"
               aria-label="Follow on Instagram"
-              icon={DEEZIcon}
+              icon={InstagramIcon}
             />
             <SocialLink
               href="https://github.com"
               aria-label="Follow on GitHub"
-              icon={DEEZIcon}
+              icon={GitHubIcon}
             />
             <SocialLink
               href="https://linkedin.com"
               aria-label="Follow on LinkedIn"
-              icon={DEEZInIcon}
+              icon={LinkedInIcon}
             />
           </div>
         </div>
